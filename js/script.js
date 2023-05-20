c45dcee475f95d0f7c58b7d9da42a9a9f655a466
funmi_bam <funmioluwafemi19@gmail.com>
2023-05-20 11:47:00 -0700

    Made changes on js/scrit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 var button = document.querySelector(".show-cat");
-// console.log(button);
-
 var cat =document.querySelector(".cat");
+// console.log(button);
 // console.log(cat);
+
 //button.innerText="it is working";  
-button.addEventListener("click", function(){
- cat.classList.add("show"); 
-})
+
+//  When user clicks on the button, function is triggered to show cat image
+button.addEventListener("click", function () {
+    if (cat.classList.contains("show")) {
+        // console.log("Yes");
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!";
+        button.classList.add("disappear");
+    } else {
+     //   console.log("No");
+     cat.classList.add("show");
+     button.innerText = "Shoo, cat!";
+     button.classList.remove("disappear");
+    }
+  });
+  
